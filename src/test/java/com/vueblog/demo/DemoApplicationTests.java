8546f54731d5a67b0c3e19bb1a98b9a9d3362bd0
liumeng <lm65589@163.com>
2020-10-22 13:38:12 +0800

    Feature:000; Subject: 完善代码;

--- src/test/java/com/vueblog/demo/DemoApplicationTests.java
+++ src/test/java/com/vueblog/demo/DemoApplicationTests.java
@@ -1,7 +1,6 @@
 package com.vueblog.demo;
 
-//import org.junit.jupiter.api.Test;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
