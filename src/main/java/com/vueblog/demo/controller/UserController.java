b4afa5d6790e7df79026a6ee8a6b1d90b0fc1b43
liumeng <lm65589@163.com>
2020-10-27 11:41:25 +0800

    Feature:000; Subject: 晚上login接口;

--- src/main/java/com/vueblog/demo/controller/UserController.java
+++ src/main/java/com/vueblog/demo/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.vueblog.demo.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.vueblog.demo.common.lang.Result;
+import com.vueblog.demo.entity.User;
+import com.vueblog.demo.service.UserService;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -16,5 +20,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    UserService userService;
 
+//    @RequiresAuthentication
+    @GetMapping("/index")
+    public Result index() {
+        User user = userService.getById(1L);
+        return Result.succ(user);
+    }
+
+    @PostMapping("/save")
+    public Result save(@Validated @RequestBody User user) {
+        return Result.succ(user);
+    }
 }
